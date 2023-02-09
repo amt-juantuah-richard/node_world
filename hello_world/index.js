50c0b9dd367a86e63165f04ef0e1a3bf4cce04c3
mrbridge-dev <oseijuantuah@gmail.com>
2023-02-09 17:46:32 +0000

    hellow

---
+++ hello_world/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
+
+app.listen(port, () => {
+  console.log(`app is running on http://localhost:${port}`)
+})
