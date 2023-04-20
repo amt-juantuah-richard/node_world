db1505eddbbabd58b97a56d4f6cd35cd38a54138
Richard Osei Juantuah <oseijuantuah@gmail.com>
2023-04-20 18:32:31 +0000

    Update dtb.ts

--- file-server-typescript-node-postgres/Server/src/dtb.ts
+++ file-server-typescript-node-postgres/Server/src/dtb.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
 export default new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'filesdb',
-    password: 'postgres',
-    port: 5432
+    user: process.env.DB_USER | 'postgres',
+    host: process.env.DB_HOST | 'localhost',
+    database: process.env.DB_DBNAME | 'filesdb',
+    password: process.env.DB_PASS | 'postgres',
+    port: process.env.DB_PORT | 5432
 })
