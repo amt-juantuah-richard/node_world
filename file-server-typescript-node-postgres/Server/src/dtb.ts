f861ed9b686aeb2356d455bbf49ffb6dc2537734
mrbridge-dev <oseijuantuah@gmail.com>
2023-04-20 19:30:15 +0000

    alter db config

--- file-server-typescript-node-postgres/Server/src/dtb.ts
+++ file-server-typescript-node-postgres/Server/src/dtb.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
 export default new Pool({
-    user: process.env.DB_USER | 'postgres',
-    host: process.env.DB_HOST | 'localhost',
-    database: process.env.DB_DBNAME | 'filesdb',
-    password: process.env.DB_PASS | 'postgres',
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_DBNAME || 'filesdb',
+    password: process.env.DB_PASS || 'postgres',
     port: 5432
 })
