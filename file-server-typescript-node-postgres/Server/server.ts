f6dd64803c0784dd69f0181bf3fce748cc5a2522
Richard Osei Juantuah <oseijuantuah@gmail.com>
2023-04-21 09:05:59 +0000

    Update server.ts

--- file-server-typescript-node-postgres/Server/server.ts
+++ file-server-typescript-node-postgres/Server/server.ts
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
       const origin = req.headers.origin;
       if (origin && allowedOrigins.includes(origin)) {
            res.setHeader('Access-Control-Allow-Origin', origin);
-           res.setHeader('Access-Control-Allow-Credentials', true)
+           res.setHeader('Access-Control-Allow-Credentials', 'true');
       }
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
