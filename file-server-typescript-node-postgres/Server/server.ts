b7c89130676f6f9a68200c280767cf12186ffd1a
Richard Osei Juantuah <oseijuantuah@gmail.com>
2023-04-21 07:44:01 +0000

    Update server.ts

--- file-server-typescript-node-postgres/Server/server.ts
+++ file-server-typescript-node-postgres/Server/server.ts
@@ -25,9 +25,19 @@ app.use(express.static(FILES));
 // general middlewares for all endpoints:
 // allow json parsing
 // allow cors origin
+app.use(function(req, res, next) {
+      // res.header("Access-Control-Allow-Origin", "*");
+      const allowedOrigins = ['http://localhost:3000', 'http://document-hub-store.onrender.com', 'https://document-hub-store.onrender.com', 'https://documenthub.vercel.app'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+           res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      res.header("Access-Control-Allow-credentials", true);
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+      next();
+    });
 app.use(express.json());
-app.use(cors());
-app.use(cors({ origin: ['http://localhost:3000', 'http://document-hub-store.onrender.com', 'https://document-hub-store.onrender.com'], credentials: true }))
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
