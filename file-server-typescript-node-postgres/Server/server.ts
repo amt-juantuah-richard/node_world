dda7bfdddd7b449fd7c49b9aee2bb2585d5afea0
Richard Osei Juantuah <oseijuantuah@gmail.com>
2023-04-21 07:50:39 +0000

    Update server.ts

--- file-server-typescript-node-postgres/Server/server.ts
+++ file-server-typescript-node-postgres/Server/server.ts
@@ -29,11 +29,10 @@ app.use(function(req, res, next) {
       // res.header("Access-Control-Allow-Origin", "*");
       const allowedOrigins = ['http://localhost:3000', 'http://document-hub-store.onrender.com', 'https://document-hub-store.onrender.com', 'https://documenthub.vercel.app'];
       const origin = req.headers.origin;
-      if (allowedOrigins.includes(origin)) {
+      if (origin && allowedOrigins.includes(origin)) {
            res.setHeader('Access-Control-Allow-Origin', origin);
       }
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-      res.header("Access-Control-Allow-credentials", true);
       res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
       next();
     });
