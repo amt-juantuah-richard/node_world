54cd277abe4600c46629251f87a179ab77b0a550
Richard Osei Juantuah <oseijuantuah@gmail.com>
2023-04-25 02:25:29 +0000

    update controller

--- file-server-typescript-node-postgres/Server/src/serveFiles/controller.ts
+++ file-server-typescript-node-postgres/Server/src/serveFiles/controller.ts
@@ -485,7 +485,7 @@ export const sendFileAsMail = async (req: Request, res: Response, next: NextFunc
             secure: true,
             auth: {
                 user: "documenthubmailer@gmail.com",
-                pass: 'jgacqetnoneugsez',
+                pass: 'az89good',
             }
         });
         let info = await transporter.sendMail({
